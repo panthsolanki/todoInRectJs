07ce9e42bef4fed4ecd8d94fd060165cf24952a4
panth <panth.solanki@gmail.com>
2019-02-07 20:42:12 +0530

    Router added

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About(){
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is about page of Todo List</p>
+    </React.Fragment>
+  )
+}
+
+export default About
